b824866ea79cf470d64d184659efb52ba25a4b99
Falguni Das <dasshuvo7@gmail.com>
2020-06-05 19:43:50 +0600

    Modify sample method to return arrays of s,a,r,s2,term

--- transition_table.py
+++ transition_table.py
@@ -1,5 +1,6 @@
 from collections import deque
 import random
+import numpy as np
 
 
 class TransitionTable(object):
@@ -8,7 +9,22 @@ class TransitionTable(object):
 
     def sample(self, size=1):
         assert len(self.transitions) >= size
-        return random.sample(self.transitions, size)
+        samples = random.sample(self.transitions, size)
+
+        s = np.empty(shape=(size, 84, 84, 4))
+        s2 = np.empty(shape=(size, 84, 84, 4))
+        a = np.empty(shape=size)
+        r = np.empty(shape=size)
+        term = np.empty(shape=size)
+
+        for i in range(size):
+            s[i] = samples[i][0]
+            a[i] = samples[i][1]
+            r[i] = samples[i][2]
+            s2[i] = samples[i][3]
+            term[i] = samples[i][4]
+
+        return s, a, r, s2, term
 
     def add(self, s, a, r, s2, is_term):
         term = 1 if is_term else 0

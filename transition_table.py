e8c711f511bc26caadb7f6163ed1e059cc670966
Falguni Das <dasshuvo7@gmail.com>
2020-06-07 15:37:30 +0600

    Refactor dtypes of np arrays and tf tensors to proper values

--- transition_table.py
+++ transition_table.py
@@ -8,16 +8,15 @@ class TransitionTable(object):
     def __init__(self, maxlen=1000000):
         self.transitions = deque(maxlen=maxlen)
 
-    @tf.function
     def sample(self, size=1):
         assert len(self.transitions) >= size
         samples = random.sample(self.transitions, size)
 
-        s = np.empty(shape=(size, 84, 84, 4))
-        s2 = np.empty(shape=(size, 84, 84, 4))
-        a = np.empty(shape=size)
-        r = np.empty(shape=size)
-        term = np.empty(shape=size)
+        s = np.empty(shape=(size, 84, 84, 4), dtype=np.float32)
+        s2 = np.empty(shape=(size, 84, 84, 4), dtype=np.float32)
+        a = np.empty(shape=size, dtype=np.int32)
+        r = np.empty(shape=size, dtype=np.float32)
+        term = np.empty(shape=size, dtype=np.float32)
 
         for i in range(size):
             s[i] = samples[i][0]
@@ -26,11 +25,11 @@ class TransitionTable(object):
             s2[i] = samples[i][3]
             term[i] = samples[i][4]
 
-        s = tf.Variable(s)
-        a = tf.Variable(a)
-        r = tf.Variable(r)
-        s2 = tf.Variable(s2)
-        term = tf.Variable(term)
+        s = tf.Variable(s, dtype=tf.float32)
+        a = tf.Variable(a, dtype=tf.int32)
+        r = tf.Variable(r, dtype=tf.float32)
+        s2 = tf.Variable(s2, dtype=tf.float32)
+        term = tf.Variable(term, dtype=tf.float32)
 
         return s, a, r, s2, term
 

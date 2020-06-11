b769ec3a99511a4e756c9c969b19f34183debd6f
Falguni Das <dasshuvo7@gmail.com>
2020-06-11 15:23:34 +0600

    Modify code

--- atari_tester.py
+++ atari_tester.py
@@ -14,10 +14,10 @@ if __name__ == '__main__':
     env = ALEManager(env_args)
     agent_args = DQLAgentArgs()
     agent_args.env = env
-    epsilon = 0.5
+    epsilon = 0.05
 
     try:
-        dql_agent = tf.keras.models.load_model('./models/815864saved_from_finally_block-h5')
+        dql_agent = tf.keras.models.load_model('./models/2162784saved_from_finally_block-h5')
     except Exception as e:
         print(e)
         raise
@@ -25,17 +25,27 @@ if __name__ == '__main__':
     for i in range(10):
         obs = env.initialize_input_sequence()
         return_val = 0
+        actions = []
         while True:
             if env.is_game_over():
                 print("Return of Episode " + str(i) + " is: ", return_val)
+                # print("actions:", np.unique(np.array(actions)))
+                # print(env.env.unwrapped.get_action_meanings())
+                # time.sleep(3)
                 break
 
             if random.random() <= epsilon:
                 action = env.get_random_action()
             else:
                 action = np.argmax(dql_agent.predict(np.expand_dims(obs, 0))[0])
+                # actions.append(action)
+                # print(action)
+                # print(dql_agent.predict(np.expand_dims(obs, 0)))
+                # time.sleep(1)
+
+            # print(action)
 
             reward, obs = env.execute_action(action)
             return_val += reward
 
-            #time.sleep(0.5)
+            # time.sleep(0.5)
